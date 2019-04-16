a267b0864d73000ad45c6a45ac54d50b98094967
boy.lee <boy.lee.here@gmail.com>
2019-04-16 14:14:43 +0800

    upgrade file ext name check logic, it's a safe upgrade for old project

--- common/eeUploadedFile.php
+++ common/eeUploadedFile.php
@@ -12,9 +12,21 @@ namespace eeTools\common;
  */
 
 use yii\web\UploadedFile;
+use yii\helpers\BaseFileHelper;
 
 class eeUploadedFile extends UploadedFile{
+    
+    public $useMimeExt = false;
+    public $_ext = '';
     public $newName;
+    
+    public function init() {
+        parent::init();
+        
+        //new property for writable.
+        $this->_ext = $this->extension;
+        
+    }
     
     /**
      * move upload file with default path, to cover about 80% conditions
@@ -29,19 +41,45 @@ class eeUploadedFile extends UploadedFile{
             $path = 'site/';
         }
         
+        //extension check bye mime type
+        if ($this->useMimeExt) {
+            $this->checkMimeExtension();
+        }
+        
         // random name
         $this->newName = $fileName;
         
-        //check extension
-        $tmpArr = explode('.', $this->newName);
-        if (!empty($tmpArr) && $this->extension != end($tmpArr)) {
-            $this->newName = str_replace('.'.end($tmpArr), '.'.$this->extension, $this->newName);
-        }//auto switch extension name.
         
         if (empty($fileName)) {
-            $this->newName = $path . eeString::randomString ( 10, 1, 2, '_' ) . '.' . $this->extension;
+            $this->newName = $path . eeString::randomString ( 10, 1, 2, '_' ) . '.' . $this->_ext;
+        }else{
+            //check extension for exist file name
+            $tmpArr = explode('.', $this->newName);
+            if (!empty($tmpArr) && $this->_ext != end($tmpArr)) {
+                $this->newName = str_replace('.'.end($tmpArr), '.'.$this->_ext, $this->newName);
+            }//auto switch extension name.
         }
         
         return $this->saveAs ( $basedPath . $this->newName );
+    }
+    
+    /**
+     * get extension name from mime type, the true extension name.
+     */
+    public function checkMimeExtension() {
+    
+        $mimeType = BaseFileHelper::getMimeType($this->tempName);
+    
+        $extensionName = 'eex';
+    
+        $mimeArr = [];
+        $mimeArr['image/jpg'] = 'jpg';
+        $mimeArr['image/png'] = 'png';
+        $mimeArr['image/jpeg'] = 'jpeg';
+    
+        if (isset($mimeArr[$mimeType])) {
+            $extensionName = $mimeArr[$mimeType];
+        }
+        $this->_ext = $extensionName;
     }
 }
